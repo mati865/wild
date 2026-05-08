mati865/wild

commit 93edc869a9d52a1b9ac80e0dce375a3199e204b3

Support running on wasi (#1723)
Fixes #1462

Co-authored-by: David Lattimore <dvdlttmr@gmail.com>

libwild/src/args.rs
@@ -215,9 +215,13 @@ impl CommonArgs {
         });
 
         // The pool might be already initialized, suppress the error intentionally.
-        let _ = ThreadPoolBuilder::new()
-            .num_threads(self.available_threads.get())
-            .build_global();
+        if self.available_threads.get() <= 1 {
+            let _ = ThreadPoolBuilder::new().use_current_thread().build_global();
+        } else {
+            let _ = ThreadPoolBuilder::new()
+                .num_threads(self.available_threads.get())
+                .build_global();
+        }
 
         Ok(ThreadPool {
             _jobserver_tokens: tokens,

.github/workflows/ci.yml
@@ -212,6 +212,24 @@ jobs:
       - run: cp .cargo/config.toml.cross-loongarch64 .cargo/config.toml
       - run: cargo build --target loongarch64-unknown-linux-gnu
 
+  wasip1-build:
+    name: wasip1 build
+    runs-on: ubuntu-24.04
+    timeout-minutes: 10
+    container:
+      image: ubuntu:25.10
+    steps:
+      - uses: actions/checkout@v6
+        with:
+          persist-credentials: false
+      - run: apt-get install --update -y build-essential gcc gcc-multilib clang curl
+      - uses: dtolnay/rust-toolchain@stable
+        id: rust-toolchain
+        with:
+          targets: wasm32-wasip1
+      - uses: Swatinem/rust-cache@v2
+      - run: cargo build --target wasm32-wasip1
+
   spelling:
     name: Spell Check with Typos
     runs-on: ubuntu-24.04

libwild/src/arch.rs
@@ -12,6 +12,7 @@ pub(crate) enum Architecture {
     AArch64,
     RISCV64,
     LoongArch64,
+    Unsupported,
 }
 
 impl TryFrom<u16> for Architecture {
@@ -35,6 +36,7 @@ impl Display for Architecture {
             Architecture::AArch64 => "aarch64",
             Architecture::RISCV64 => "riscv64",
             Architecture::LoongArch64 => "loongarch64",
+            Architecture::Unsupported => "unsupported",
         };
         write!(f, "{arch}")
     }

libwild/src/args/elf.rs
@@ -309,20 +309,23 @@ const fn default_target_arch() -> Architecture {
     // architectures that we can't target.
     #[cfg(target_arch = "x86_64")]
     {
-        Architecture::X86_64
+        return Architecture::X86_64;
     }
     #[cfg(target_arch = "aarch64")]
     {
-        Architecture::AArch64
+        return Architecture::AArch64;
     }
     #[cfg(target_arch = "riscv64")]
     {
-        Architecture::RISCV64
+        return Architecture::RISCV64;
     }
     #[cfg(target_arch = "loongarch64")]
     {
-        Architecture::LoongArch64
+        return Architecture::LoongArch64;
     }
+
+    #[allow(unreachable_code)]
+    Architecture::Unsupported
 }
 
 impl ElfArgs {
@@ -1879,6 +1882,7 @@ impl platform::Args for ElfArgs {
             Architecture::AArch64 => Alignment { exponent: 16 },
             Architecture::RISCV64 => Alignment { exponent: 12 },
             Architecture::LoongArch64 => Alignment { exponent: 16 },
+            Architecture::Unsupported => unreachable!(),
         }
     }
 

libwild/src/elf.rs
@@ -333,6 +333,12 @@ impl platform::Platform for Elf {
             crate::arch::Architecture::LoongArch64 => {
                 linker.link_for_arch::<Elf, crate::elf_loongarch64::ElfLoongArch64>(args)
             }
+            crate::arch::Architecture::Unsupported => {
+                bail!(
+                    "No default target architecture known for host platform. \
+                    Please specify an architecture with -m"
+                )
+            }
         }
     }
 

libwild/src/fs.rs
@@ -16,7 +16,7 @@ pub(crate) fn make_executable(_file: &File) -> Result {
         Ok(())
     }
 
-    #[cfg(windows)]
+    #[cfg(not(unix))]
     {
         Ok(())
     }
@@ -35,5 +35,12 @@ pub(crate) fn path_from_bytes(bytes: &[u8]) -> PathBuf {
         use std::path::PathBuf;
         let path = std::str::from_utf8(bytes).expect("Invalid UTF-8 in archive path name");
         PathBuf::from(path)
+    }
+
+    #[cfg(target_os = "wasi")]
+    {
+        use std::ffi::OsStr;
+        use std::os::wasi::ffi::OsStrExt as _;
+        std::path::Path::new(OsStr::from_bytes(bytes)).to_path_buf()
     }
 }

libwild/src/input_data.rs
@@ -24,12 +24,14 @@ use crate::verbose_timing_phase;
 use colosseum::sync::Arena;
 use crossbeam_queue::SegQueue;
 use hashbrown::HashMap;
+#[cfg(not(target_family = "wasm"))]
 use memmap2::Mmap;
 use rayon::Scope;
 use rayon::iter::IntoParallelIterator;
 use rayon::iter::IntoParallelRefIterator;
 use rayon::iter::ParallelIterator;
 use std::fmt::Display;
+use std::fs::File;
 use std::ops::Deref;
 use std::path::Path;
 use std::path::PathBuf;
@@ -93,11 +95,33 @@ pub(crate) struct InputFile {
 
 #[derive(Debug)]
 pub(crate) struct FileData {
-    bytes: Mmap,
+    bytes: FileBytes,
 
     /// The modification timestamp of the input file just before we opened it. We expect our input
     /// files not to change while we're running.
     modification_time: std::time::SystemTime,
+}
+
+#[cfg(not(target_family = "wasm"))]
+#[derive(Debug)]
+struct FileBytes(Mmap);
+
+#[cfg(target_family = "wasm")]
+struct FileBytes(Vec<u8>);
+
+#[cfg(target_family = "wasm")]
+impl std::fmt::Debug for FileBytes {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("FileBytes").finish_non_exhaustive()
+    }
+}
+
+impl Deref for FileBytes {
+    type Target = [u8];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
 /// Identifies an input object that may not be a regular file on disk, or may be an entry in an
@@ -787,46 +811,65 @@ impl FileData {
     }
 
     fn open(path: &Path, prepopulate_maps: bool) -> Result<(Self, std::fs::File)> {
-        let file = std::fs::File::open(path)
+        let mut file = std::fs::File::open(path)
             .with_context(|| format!("Failed to open input file `{}`", path.display()))?;
 
-        let modification_time = std::fs::metadata(path)
+        let modification_time = file
+            .metadata()
             .and_then(|meta| meta.modified())
             .with_context(|| {
                 format!("Failed to read file modification time `{}`", path.display())
             })?;
 
-        // Safety: Unfortunately, this is a bit of a compromise. Basically this is only safe if our
-        // users manage to avoid editing the input files while we've got them mapped. It'd be great
-        // if there were a way to protect against unsoundness when the input files were modified
-        // externally, but there isn't - at least on Linux. Not only could the bytes change without
-        // notice, but the mapped file could be truncated causing any access to result in a SIGBUS.
-        //
-        // For our use case, mmap just has too many advantages. There are likely large parts of our
-        // input files that we don't need to read, so reading all our input files up front isn't
-        // really an option. Reading just the parts we need might be an option, but would add
-        // substantial complexity. Also, using mmap means that if the system needs to reclaim
-        // memory, it can just release some of our pages.
-
-        let mut mmap_options = memmap2::MmapOptions::new();
-
-        // Prepopulating maps generally slows things down, so is off by default, however it's useful
-        // when profiling, since it means that you don't see false positive slowness in the parts of
-        // the code that first read a bit of memory.
-        if prepopulate_maps {
-            mmap_options.populate();
-        }
-
-        let bytes = unsafe { mmap_options.map(&file) }
-            .with_context(|| format!("Failed to mmap input file `{}`", path.display()))?;
-
         Ok((
             FileData {
-                bytes,
+                bytes: FileBytes::read(&mut file, path, prepopulate_maps)?,
                 modification_time,
             },
             file,
         ))
+    }
+}
+
+impl FileBytes {
+    #[cfg(not(target_family = "wasm"))]
+    fn read(file: &mut File, path: &Path, prepopulate_maps: bool) -> Result<Self> {
+        // Safety: Unfortunately, this is a bit of a compromise. Basically this is only safe if
+        // our users manage to avoid editing the input files while we've got them
+        // mapped. It'd be great if there were a way to protect against unsoundness
+        // when the input files were modified externally, but there isn't - at least
+        // on Linux. Not only could the bytes change without notice, but the mapped
+        // file could be truncated causing any access to result in a SIGBUS.
+        //
+        // For our use case, mmap just has too many advantages. There are likely large parts of
+        // our input files that we don't need to read, so reading all our input
+        // files up front isn't really an option. Reading just the parts we need
+        // might be an option, but would add substantial complexity. Also, using
+        // mmap means that if the system needs to reclaim memory, it can just
+        // release some of our pages.
+
+        let mut mmap_options = memmap2::MmapOptions::new();
+
+        // Prepopulating maps generally slows things down, so is off by default, however it's
+        // useful when profiling, since it means that you don't see false positive
+        // slowness in the parts of the code that first read a bit of memory.
+        if prepopulate_maps {
+            mmap_options.populate();
+        }
+
+        let bytes = unsafe { mmap_options.map(&*file) }
+            .with_context(|| format!("Failed to mmap input file `{}`", path.display()))?;
+
+        Ok(FileBytes(bytes))
+    }
+
+    #[cfg(target_family = "wasm")]
+    fn read(file: &mut File, path: &Path, _prepopulate_maps: bool) -> Result<Self> {
+        use std::io::Read;
+        let mut bytes = vec![];
+        file.read_to_end(&mut bytes)
+            .with_context(|| format!("Failed to read file `{}`", path.display()))?;
+        Ok(FileBytes(bytes))
     }
 }
 

libwild/src/save_dir.rs
@@ -277,7 +277,13 @@ impl SaveDirState {
                 self.copy_file(&absolute_target, parsed_args)?;
             }
 
-            create_symlink(&target, &dest_path)?;
+            if let Err(error) = create_symlink(&target, &dest_path) {
+                // If we can't create a symlink, then fall back to copying. If that fails, then
+                // return the error from when we tried to create the symlink.
+                if std::fs::copy(&target, &dest_path).is_err() {
+                    return Err(error);
+                }
+            }
         } else {
             if let Ok(data) = FileData::new(source_path, false) {
                 match FileKind::identify_bytes(&data) {
@@ -390,6 +396,11 @@ fn create_symlink(target: &Path, dest_path: &Path) -> Result {
             )
         })?;
         Ok(())
+    }
+    #[cfg(target_os = "wasi")]
+    {
+        let _ = (target, dest_path);
+        bail!("creating symlinks on wasi not supported on stable rust");
     }
 }
 

commit 9720bf50a24267b45dde8eb0a475b6ba881f49fc

fix(jobserver): ThreadPoolBuilder must use 1 thread with available_threads == 1 (#1904)
Noticed it's a regression since #1723, where with a very parallel
set-up, the `jobserver` gets just a single thread, but the missing
`.num_threads(1)` effectively leads to using all the available threads.
Which is incorrect.

Apart from that, I'm simplifying the code.

CC: @bjorn3

libwild/src/args.rs
@@ -327,15 +327,18 @@ impl CommonArgs {
                 }
                 tracing::trace!(count = tokens.len(), "Acquired jobserver tokens");
                 // Our parent "holds" one jobserver token, add it.
-                NonZeroUsize::new((tokens.len() + 1).max(1)).unwrap()
+                NonZeroUsize::new(tokens.len() + 1).unwrap()
             } else {
                 std::thread::available_parallelism().unwrap_or(NonZeroUsize::new(1).unwrap())
             }
         });
 
         // The pool might be already initialized, suppress the error intentionally.
-        if self.available_threads.get() <= 1 {
-            let _ = ThreadPoolBuilder::new().use_current_thread().build_global();
+        if self.available_threads.get() == 1 {
+            let _ = ThreadPoolBuilder::new()
+                .use_current_thread()
+                .num_threads(1)
+                .build_global();
         } else {
             let _ = ThreadPoolBuilder::new()
                 .num_threads(self.available_threads.get())